70530ce6e7ca19a331abba0585099b0e4354f7d1
ThangNguyen <thangnh.pb@gmail.com>
2025-02-02 21:48:41 +0700

    Template engine

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const { engine } = require('express-handlebars');
+const app = express()
+const port = 2101
+
+//HTTP logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+//route định nghĩa tuyên đường dẫn trang chính
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
