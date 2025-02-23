aae734983224567f2b7d2138666e509922ae382b
ThangNguyen <thangnh.pb@gmail.com>
2025-02-23 10:30:05 +0700

    Post method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express') 
 const morgan = require('morgan')
 const path = require('path')
 const { engine } = require('express-handlebars');
@@ -6,16 +6,20 @@ const app = express()
 const port = 2101
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 //HTTP logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 //Template engine
 app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 //route định nghĩa tuyên đường dẫn trang chính
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.render('home')
 })
 
@@ -23,6 +27,17 @@ app.get('/news', (req, res) => {
   res.render('news')
 })
 
+app.get('/search', (req, res) => {
+  // console.log(req.query)
+  res.render('search')
+})
+
+app.post('/search', (req, res) => {
+
+  console.log(req.body)
+
+  res.send('')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
